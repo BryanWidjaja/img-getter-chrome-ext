fb8ad32e8cd8759682961802dd2f89fce8f13242
Bryan Widjaja <crz.github@gmail.com>
2026-02-03 16:49:36 +0700

    feat : tag generation test, tags container, copy btn

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
+const copyBtn = document.getElementById("copy-btn");
+let tags = "";
+
 document.getElementById("getImages").addEventListener("click", async () => {
-  const imagesDiv = document.getElementById("images");
-  imagesDiv.innerHTML = "";
+  // const imagesDiv = document.getElementById("images");
+  // imagesDiv.innerHTML = "";
 
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   if (!tabs.length) {
-    imagesDiv.textContent = "No active tab";
+    // imagesDiv.textContent = "No active tab";
     return;
   }
 
@@ -23,25 +26,42 @@ document.getElementById("getImages").addEventListener("click", async () => {
     });
 
     if (!results || !results.length || !results[0].result) {
-      imagesDiv.textContent = "Image not found";
+      // imagesDiv.textContent = "Image not found";
       return;
     }
 
-    const src = results[0].result;
+    // const src = results[0].result;
 
-    const link = document.createElement("a");
-    link.href = "#";
-    link.textContent = src;
-    link.style.display = "block";
+    // const link = document.createElement("a");
+    // link.href = "#";
+    // link.textContent = src;
+    // link.style.display = "block";
 
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
-      chrome.tabs.create({ url: src, active: false });
-    });
+    // link.addEventListener("click", (e) => {
+    //   e.preventDefault();
+    //   chrome.tabs.create({ url: src, active: false });
+    // });
 
-    imagesDiv.appendChild(link);
+    // imagesDiv.appendChild(link);
+    fetch("../output.json")
+      .then((res) => res.json())
+      .then((data) => {
+        tags = data.output;
+        document.getElementById("tags").textContent = tags;
+      })
+      .catch((error) => console.error("Fetch error : ", error));
   } catch (err) {
     console.error(err);
-    imagesDiv.textContent = "Cannot access this page";
+    // imagesDiv.textContent = "Cannot access this page";
   }
+});
+
+copyBtn.addEventListener("click", async () => {
+  if (!tags) {
+    alert("No tags to copy yet");
+    return;
+  }
+
+  await navigator.clipboard.writeText(tags);
+  alert("Copied!");
 });
